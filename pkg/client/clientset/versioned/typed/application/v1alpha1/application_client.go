e779149f819b903c40f54c12cdfbd3b2b744f6bc
mensylisir <710080675@qq.com>
2023-07-18 17:23:00 +0800

    Update application_client.go

--- pkg/client/clientset/versioned/typed/application/v1alpha1/application_client.go
+++ pkg/client/clientset/versioned/typed/application/v1alpha1/application_client.go
@@ -20,8 +20,8 @@ package v1alpha1
 
 import (
 	rest "k8s.io/client-go/rest"
-	v1alpha1 "kubesphere.io/api/application/v1alpha1"
-	"kubesphere.io/kubesphere/pkg/client/clientset/versioned/scheme"
+	v1alpha1 "github.com/mensylisir/api/application/v1alpha1"
+	"github.com/mensylisir/kubesphere/pkg/client/clientset/versioned/scheme"
 )
 
 type ApplicationV1alpha1Interface interface {
